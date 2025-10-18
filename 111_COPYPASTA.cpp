702045c3db28aaa3a35719acd2c175affd316202
kusic <chenglijun68@gmail.com>
2025-10-18 16:27:46 +0800

    Add files via upload
    added root file with macros

---
+++ 111_COPYPASTA.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+#undef ll
+#define ll long long
+#undef LL_MAX
+#define LL_MAX 9223372036854775807
+#undef INT_MAX
+#define INT_MAX 2147483647
+#undef F
+#define F first
+#undef S
+#define S second
+#undef P
+#define P pair<int,int>
+#define v(l) vector<l>
+#define lp(n) for(int i=0;i<n;i++)
+#define pb push_back
+using namespace std;
+int main(){
+    lp(10){
+        cout<<i<<endl;
+    }
+}
